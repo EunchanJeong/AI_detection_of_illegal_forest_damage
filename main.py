d7194ad962068025c5596cdc8c7c683bb743d339
Heyong Jun Kim <81418195+brojoon1@users.noreply.github.com>
2023-11-07 12:34:18 +0900

    Fix: main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import torch
 vgg_model = load_model('model_vgg_2500.h5')
 
 # YOLO 모델 불러오기
-yolo_model = load_model('model_yolo.pt')
+#yolo_model = load_model('model_yolo.pt')
 
 # 타이틀
 st.markdown("<h1 style='text-align: center; color: green;'>그린</h1>", unsafe_allow_html=True)
